9cc6504e1a2988f7198f95a6f7cd53ec57352ab6
Shahabkhan6699 <168974573+Shahabkhan6699@users.noreply.github.com>
2024-12-24 17:03:31 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'e6ad1562-532f-4b06-b85a-b91a4aa2c2c8';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
